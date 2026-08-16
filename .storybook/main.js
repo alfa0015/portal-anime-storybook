23f23a4bb6e077c351968f240db15eee6500bfa1
Rafael Alfaro <alfa0015.rafael@gmail.com>
2026-08-16 13:58:47 -0600

    feat: update Storybook configuration and add MCP addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,7 @@ const config = {
     '@storybook/addon-a11y',
     '@storybook/addon-docs',
     '@storybook/addon-onboarding',
+    '@storybook/addon-mcp',
   ],
   framework: '@storybook/vue3-vite',
   core: {
